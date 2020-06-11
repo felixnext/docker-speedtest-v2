35998dc7f3ca33db06b5a4dd046d5914883ce03c
Felix Geilert <root92@live.de>
2020-06-11 11:07:05 +0200

    Feat: Created basic structure - implemented first version of speedtest - added structure for webapp - created rough docker structure

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # docker-speedtest-v2
-New Version of the docker speed-test tool
+
+New version of the docker speedtest tool. The components are more decoupled and the UI is reworked to allow manual tests and more data.
+
+## Getting Started
+
+First build the docker containers:
+
+```bash
+cd speedtest
+docker build -t speedtest .
+cd ../webapp
+docker build -t speedtest-app .
+cd ..
+```
+
+You can then run the docker compose script:
+
+```bash
+docker-compose 
+```
+
+If you want to run the compose script always on startup:
+
+```bash
+docker-compose
+```
+
+## License
+
+The code is published under the Apache-2 License.
+
+## Contribution
+
+If you want to contribute, feel free to create a PR
---
+++ speedtest/speedtest.py
@@ -0,0 +1,141 @@
+'''
+Simple Script that runs the speedtest in periodic time intervals
+'''
+
+import speedtest as speed
+import fire
+from time import time, sleep
+import psycopg2 as psy
+
+class Tester():
+    def __init__(self, host, port="5432"):
+        # connect to the database
+        self.host = host
+        self.port = port
+        self.connect()
+
+        # defines the time interval in which checks should be done
+        self.interval = 30
+        self.flag_test = False
+
+        # update data
+        self.check_flags()
+    
+    def connect(self):
+        '''Establishes connection to database.'''
+        self.conn = None
+        while (self.conn != None):
+            try:
+                self.conn = psy.connect(database="speedtest", user="postgres", password="pwd", host=self.host, port=self.port)
+            except:
+                print("WARNING: Unable to database, retry in 30 seconds")
+                sleep(30)
+        print("Connected to database")
+
+    def check_connection(self):
+        '''Checks the database connection and eventually resets it.'''
+        try:
+            cur = self.conn.cursor()
+            cur.execute('SELECT 1')
+            cur.close()
+        except psy.OperationalError:
+            self.connect()
+
+    def run(self):
+        '''Defines the main run cycle.'''
+        run = True
+
+        # main execution loop
+        while(run):
+            try:
+                # check connection and run speedtest
+                self.check_connection()
+                self.collect()
+
+                # map time and check difference until reached
+                last_check = time()
+                while (time() - last_check) < self.interval:
+                    # check current flags
+                    self.check_flags()
+
+                    # check for test
+                    if self.flag_test is True:
+                        self.collect()
+                        self.flag_test = False
+                        self.update_flag("run_test", "false")
+                    
+                    # wait
+                    sleep(10)
+            except KeyboardInterrupt:
+                print("Speedtest closed by user")
+        
+        # close the connection
+        self.conn.close()
+
+    def collect(self):
+        '''Collects a single speedtest.'''
+        results = None
+        while results is None:
+            try:
+                test = speed.Speedtest()
+                test.get_best_server()
+                test.download()
+                test.upload()
+                results = test.results.dict()
+            except:
+                print("WARNING: Speedtest failed")
+        
+        # convert to Mbits
+        for v in ["download", "upload"]:
+            results[v] = results[v] / (1024 * 1024)
+        
+        # print
+        print("INFO: Current speed is: {:.2f} MB/s download - {:.2f} MB/s upload".format(results["download"], results["upload"]))
+
+        # store the results
+        self.store(results)
+
+        return True
+    
+    def store(self, results):
+        '''Stores the speedtest into the database.'''
+        cur = self.conn.cursor()
+
+        # create insert statement
+        # TODO: add security checks that data is there?
+        cur.execute("INSERT INTO speeds (download, upload, ping, timestamp, isp, ip, country) VALUES ({}, {}, {}, '{}', '{}', '{}', '{}')".format(results["download"], results["upload"], results["ping"], results["timestamp"], results["client"]["isp"], results["client"]["ip"], results["client"]["country"]))
+        conn.commit()
+
+        cur.close()
+    
+    def check_flags(self):
+        '''Reads from the Database to check if flags are set.'''
+        cur = self.conn.cursor()
+
+        cur.execute("SELECT key, value FROM settings")
+        rows = cur.fetchall()
+        for row in rows:
+            key = row[0]
+            value = row[1].lower()
+
+            # parse settings
+            if key == "run_test":
+                self.flag_test = value == "true" or value == "1"
+            elif key == "interval":
+                self.interval = int(value)
+            else:
+                print("WARNING: unkown key {} with value {}".format(key, value))
+
+        cur.close()
+
+    def update_flag(self, key, value):
+        cur = self.conn.cursor()
+        cur.execute("INSERT INTO settings (key, value) VALUES ('{}', '{}') ON CONFLICT (key) SET value=excluded.value".format(key, value))
+        conn.commit()
+        cur.close()
+
+def main(host, port):
+    tester = Tester(host, port)
+
+if __name__ == "__main__":
+    fire.Fire(main)
