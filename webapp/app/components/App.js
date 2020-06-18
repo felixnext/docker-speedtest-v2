958b99c3ed32dca301fe7ac2bedc55e509284c1f
Felix Geilert <root92@live.de>
2020-06-18 07:48:22 +0200

    Feat: Started integrating update loops and bootstrap (+ graph)

--- webapp/app/components/App.js
+++ webapp/app/components/App.js
@@ -1,4 +1,6 @@
-import React, {useState, useEffect, Fragment} from "react";
+import React, {useState, useEffect} from "react";
+import {Button, Container, Row, Jumbotron} from 'react-bootstrap';
+import {XYPlot, LineSeries, HorizontalGridLines, VerticalGridLines, XAxis, YAxis} from 'react-vis';
 
 import styles from "../css/app.less";
 import ApiHandler from "./endpoint";
@@ -6,22 +8,64 @@ import ApiHandler from "./endpoint";
 //import 'babel-polyfill';
 
 export default function App() {
+  // --- STATES ---
   // create the relevant endpoint
   const [api, setApi] = useState(new ApiHandler());
   const [settings, setSettings] = useState({});
   const [speeds, setSpeeds] = useState([]);
+  const [tags, setTags] = useState([]);
 
-  useEffect(() => {
-    api.getSettings().then(data => setSettings(data))
-  }, [])
-
+  // --- LOGIC ---
+  // load all data initially
   useEffect(() => {
     api.getSpeeds().then(data => setSpeeds(data))
-  }, [])
+    api.getSettings().then(data => setSettings(data))
+    api.getTags().then(data => setTags(data))
+
+    const interval = setInterval(() => {
+      api.getSettings().then(data => setSettings(data))
+      api.getTags().then(data => setTags(data));
+      console.log(speeds);
+      api.getNewSpeeds(speeds[0].measure_time.toDate()).then(data => setSpeeds(speeds.concat(data)))
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [api])
+
+  // timed effect to refresh
+  useEffect(() => {
+    //
+  })
+
+  // --- RENDER ---
+  const createGraph = () => {
+    let dl = speeds.map(data => { return {x: data.measure_time.toDate(), y: data.download}; })
+    console.log(dl);
+    return (
+      <XYPlot height={500} width={1000} xType="time">
+        <HorizontalGridLines />
+        <VerticalGridLines />
+        <LineSeries data={dl} />
+        <XAxis tickFormat={function tickFormat(d){
+          const date = new Date(d)
+          return date.toISOString().substr(11, 8)
+        }} />
+        <YAxis />
+      </XYPlot>
+    );
+  }
 
   return ( 
-    <Fragment>
-      <p>WELCOME TO THE SPEEDTESTER!</p>
+    <Container>
+      <Jumbotron>
+        <h1>Welcome to the Internet Speedtester</h1>
+        <p>
+          This is a simple tool that allows you to monitor your local internet speeds.
+        </p>
+      </Jumbotron>
+      <Container>
+        {createGraph()}
+      </Container>
       <div>{Object.keys(settings).map( (key, index) => {
         return (<div key={index}>
           <span>{key}</span>
@@ -40,7 +84,7 @@ export default function App() {
           <span>{key.tags}</span>
          </div>)
       })}</div>
-    </Fragment>
+    </Container>
   );
 }
 
